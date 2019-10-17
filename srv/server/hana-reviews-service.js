fce1229bcaa77ff900fd8f4756af7139b9dcc7dc
Steffen Kötte <steffen.koette@sap.com>
2019-10-17 14:44:17 -0700

    Circuit breaker for API to DB connection
    refs #1

--- srv/server/hana-reviews-service.js
+++ srv/server/hana-reviews-service.js
@@ -1,7 +1,26 @@
 'use strict'
+const CircuitBreaker = require('opossum');
+
+const circuitBreakerOptions = {
+    timeout: 3000, // If our function takes longer than 3 seconds, trigger a failure
+    errorThresholdPercentage: 50, // When 50% of requests fail, trip the circuit
+    resetTimeout: 30000 // After 30 seconds, try again.
+  };
+
 function hanaReviewsService() {
     const dbClass = require("../utils/dbPromises");
-    this.getAll = async function (req) {
+
+    async function breaker(func, ...args) {
+        const breaker = new CircuitBreaker(func, circuitBreakerOptions);
+        var result = await breaker.fire(...args);
+        return result;
+    }
+
+    this.getAll = async function(req) {   
+        return await breaker(this._getAll, req)
+    }
+    
+    this._getAll = async function (req) {
         let db = new dbClass(req.db);
         try {
             const statement = await db.preparePromisified(`SELECT REVIEWEE_EMAIL as "reviewee_email", REVIEWER_EMAIL as "reviewer_email", RATING as "rating", COMMENT as "comment" FROM BB_REVIEWS`);
@@ -13,15 +32,22 @@ function hanaReviewsService() {
 
     }
 
-    this.getAllFor = async function (revieweeEmail, req) {
+    this.getAllFor = async function(revieweeEmail, req) {   
+        return await breaker(this._getAllFor, revieweeEmail, req)
+    }
+
+    this._getAllFor = async function (revieweeEmail, req) {
         let db = new dbClass(req.db);
         const statement = await db.preparePromisified(`SELECT REVIEWEE_EMAIL as "reviewee_email", REVIEWER_EMAIL as "reviewer_email", RATING as "rating", COMMENT as "comment" FROM BB_REVIEWS WHERE REVIEWEE_EMAIL = ?`);
         const results = await db.statementExecPromisified(statement, [revieweeEmail]);
         return results
     }
 
+    this.getAverageRating = async function(revieweeEmail, req) {   
+        return await breaker(this._getAverageRating, revieweeEmail, req)
+    }
 
-    this.getAverageRating = async function (revieweeEmail, req) {
+    this._getAverageRating = async function (revieweeEmail, req) {
         let db = new dbClass(req.db);
         const statement = await db.preparePromisified(`SELECT  avg(RATING) as "average_rating" FROM BB_REVIEWS WHERE REVIEWEE_EMAIL = ?`);
         const results = await db.statementExecPromisified(statement, [revieweeEmail]);
@@ -37,16 +63,22 @@ function hanaReviewsService() {
     }
     this.sleep = sleep;
 
-
-    this.create = async function (review, req) {
-        let db = new dbClass(req.db);
-        const statement = await db.preparePromisified(`INSERT INTO BB_REVIEWS (REVIEWEE_EMAIL, REVIEWER_EMAIL, RATING, COMMENT) values(?, ?, ?, ?)`);
-        await db.statementExecPromisified(statement, [review.reviewee_email, review.reviewer_email, review.rating, review.comment]);
-        return
-
+    this.create = async function(review, req) {   
+        return await breaker(this._create, review, req)
     }
 
-    this.deleteAll = async function (req) {
+    this._create = async function (review, req) {
+        let db = new dbClass(req.db);
+        const statement = await db.preparePromisified(`INSERT INTO BB_REVIEWS (REVIEWEE_EMAIL, REVIEWER_EMAIL, RATING, COMMENT) values(?, ?, ?, ?)`);
+        var result = await db.statementExecPromisified(statement, [review.reviewee_email, review.reviewer_email, review.rating, review.comment]);
+        return
+    }
+
+    this.deleteAll = async function(req) {   
+        return await breaker(this._deleteAll, req)
+    }
+
+    this._deleteAll = async function (req) {
         let db = new dbClass(req.db);
         const statement = await db.preparePromisified(`delete from BB_REVIEWS`);
         await db.statementExecPromisified(statement, []);
