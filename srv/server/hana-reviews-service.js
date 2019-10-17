8845d189c024d4e37069e280fc3116448ba4e689
Steffen Kötte <steffen.koette@sap.com>
2019-10-17 15:01:53 -0700

    Remove error threshold setting

--- srv/server/hana-reviews-service.js
+++ srv/server/hana-reviews-service.js
@@ -3,7 +3,6 @@ const CircuitBreaker = require('opossum');
 
 const circuitBreakerOptions = {
     timeout: 3000, // If our function takes longer than 3 seconds, trigger a failure
-    errorThresholdPercentage: 50, // When 50% of requests fail, trip the circuit
     resetTimeout: 30000 // After 30 seconds, try again.
   };
 
