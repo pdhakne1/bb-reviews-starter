ef418d39c57a8d3ccdb6be86e0ed03e878c70814
Minuchaher Havewala <anahita.minuchaher.havewala@sap.com>
2019-10-18 17:36:57 -0700

    added dummy test for pipeline

---
+++ test/testHelloWorld.js
@@ -0,0 +1,10 @@
+var assert = require('assert');
+var expect  = require('chai').expect;
+
+it('Dummy test for integration', function() {
+    assert.ok(true);
+});
+
+it('Other test for pipeline', function() {
+    assert.ok(true);
+});
